53a17dba9b02ef313b11ff639986a765f00eb7e4
Ryan Bibby <r.bibby@gmail.com>
2014-01-24 11:33:04 +0000

    Configs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 HutSample::Application.routes.draw do
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,8 +11,10 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
-
+Rails.application.routes.default_url_options[:host] = 'http://test.host'
 RSpec.configure do |config|
+  config.include Capybara::DSL
+  config.include Rails.application.routes.url_helpers
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
