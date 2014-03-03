99c3baf337f5c5b0e404ea8918b5cdc72aad1760
unknown <tmiller2@sheffield.ac.uk>
2014-03-03 12:28:59 +0000

    updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 HutSample::Application.routes.draw do
-
+Blog::Application.routes.draw do
+ 
+  resources :posts
+  root "tests#index"
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
