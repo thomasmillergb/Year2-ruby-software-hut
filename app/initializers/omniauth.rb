4cfaee79a736873487a8a0c5abad2f4564f0e0e0
Thomas Miller <tmiller2@sheffield.ac.uk>
2014-03-06 14:50:32 +0000

    OAuth with redirect URL

--- app/initializers/omniauth.rb
+++ app/initializers/omniauth.rb
@@ -2,6 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, '472391588857-lgd9u17f44m7pqnqqbbmnl7g990t6cit.apps.googleusercontent.com', '7irkIUigTVnkPeeXlm-xFacr', {
     access_type: 'offline',
     scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
-    redirect_uri:'http://localhost/auth/google_oauth2/callback'
+    redirect_uri:'http://aca11tm-demo.dev3.software-hut.org.uk/callback'
   }
 end
