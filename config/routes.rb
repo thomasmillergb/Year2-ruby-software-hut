d78eb594204dfa53b198771e04a27ffc77e40c85
aca11tm <tmiller2@sheffield.ac.uk>
2014-03-06 15:01:20 +0000

    Added rediret URL from Google to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ HutSample::Application.routes.draw do
   resources :grants
   root to: "grants#index"
 
+  #Call back route
+  match "/auth/:provider/callback" => "sessions#create"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
