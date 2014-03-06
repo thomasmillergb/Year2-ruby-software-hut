8892514306ecdc4643aaf159ac05c151fa6785f4
Thomas Miller <tmiller2@sheffield.ac.uk>
2014-03-06 15:06:49 +0000

    Git OAUTH update

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '472391588857-lgd9u17f44m7pqnqqbbmnl7g990t6cit.apps.googleusercontent.com', '7irkIUigTVnkPeeXlm-xFacr', {
+  provider :google_oauth2, '472391588857-bua65rm9o1si65ce6domdtq9iod0hr2v.apps.googleusercontent.com', 'IyplJJ93fiASMvR23R6drQog', {
     access_type: 'offline',
     scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
-    redirect_uri:'http://aca11tm-demo.dev3.software-hut.org.uk/callback'
+    redirect_uri:'http://aca11tm-demo.dev3.software-hut.org.uk/auth/google-oauth2/callback'
   }
 end
