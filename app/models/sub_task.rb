3c07d389127e0c9e8a105c76e0f6d39c322169ce
aca11tm <tmiller2@sheffield.ac.uk>
2014-03-29 13:21:08 +0000

    Added Subtasks with tasks comments, just needs to show subtasks for the current task

--- app/models/sub_task.rb
+++ app/models/sub_task.rb
@@ -1,2 +1,4 @@
 class SubTask < ActiveRecord::Base
+  belongs_to :task
+  has_many :task_comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140329130404) do
+ActiveRecord::Schema.define(version: 20140329131432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -195,6 +195,7 @@ ActiveRecord::Schema.define(version: 20140329130404) do
     t.date     "enddate"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "task_id"
   end
 
   create_table "task_comments", force: true do |t|
