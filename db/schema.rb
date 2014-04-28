34930781aefb76105dee60de9bcd2872c964f46c
aca12dp <{dpanada1@sheffield.ac.uk}>
2014-04-28 20:44:51 +0100

    added custom option to registration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140428190806) do
+ActiveRecord::Schema.define(version: 20140428194114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -367,6 +367,7 @@ ActiveRecord::Schema.define(version: 20140428190806) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "department"
   end
 
   add_index "sheffpeople", ["email"], name: "index_sheffpeople_on_email", unique: true, using: :btree
